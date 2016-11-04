b878abeb029e2b7976f018685b9671465569002b
Wingo <cmwingo@gmail.com>
2016-11-04 15:13:15 -0700

    Now add additional input matricies for users to order more pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,91 @@
 //UI Logic
 
 $(document).ready(function(){
-  $("form").submit(function(event){
+  var pizzas = [];
+  var clickCount = -1;
+  var toppings = [];
+  var size;
+
+
+  function getCheckboxInput(checkboxName){
+    var inputs = [];
+
+    $("input:checkbox[name=" + checkboxName + "]:checked").each(function(){
+      var selection = $(this).val();
+      inputs.push(selection);
+    });
+    return inputs;
+  };
+
+  $("form#order").submit(function(event){
+    alert("submisson");
     event.preventDefault();
 
-    function getCheckboxInput(checkboxName){
-      var inputs = [];
-
-      $("input:checkbox[name=" + checkboxName + "]:checked").each(function(){
-        var selection = $(this).val();
-        inputs.push(selection);
-      });
-      return inputs;
-    };
-
-    var toppings = [];
-    var size;
-    var price = 0.00;
-
-    size = $("#size").val();
-    toppings = getCheckboxInput("toppings");
+    for(i = 0; i < clickCount; i++) {
+      size = $("#size" + clickCount).val();
+      toppings = getCheckboxInput("toppings" + clickCount + "");
+      yourPizza = new Pizza(size, toppings);
+      pizzas[i] = yourPizza;
+    }
     $("#pizza-size").text(size);
     $("#pizza-toppings").text(toppings);
-    yourPizza = new Pizza(size, toppings);
     price = yourPizza.cost();
     $("#pizza-price").text(price.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
     $("#order-results").slideDown();
     console.log(price);
     console.log(size);
     console.log(toppings);
+  });
+
+  $("#add-pizza").click(function(){
+    clickCount ++;
+    size = $("#size").val();
+    toppings = getCheckboxInput("toppings");
+    var pizza = new Pizza(size, toppings);
+    pizza.price = pizza.cost(size, toppings);
+    pizzas[clickCount] = pizza;
+    console.log(pizza);
+    console.log(size);
+    console.log(toppings);
+    console.log(pizza.price);
+    $("#order").prepend(
+            '<div class="row" id="order"> \
+              <div class="col-lg-6"> \
+                <div class="form-group form-group-lg"> \
+                  <div class="panel panel-default"> \
+                    <div class="panel-heading"> \
+                      <h3 class="panel-title">Size</h3> \
+                    </div> \
+                    <div class="panel-body"> \
+                      <select id="size' + clickCount + '"> \
+                        <option value="small">Small</option> \
+                        <option value="large">Large</option> \
+                        <option value="x-large">Extra Large</option> \
+                      </select> \
+                    </div> \
+                  </div> \
+                </div> \
+              </div> \
+              <div class="col-lg-6"> \
+                <div class="form-group"> \
+                   <div class="panel panel-default"> \
+                     <div class="panel-heading"> \
+                       <h3 class="panel-title">Toppings</h3> \
+                     </div> \
+                     <div class="panel-body" id="toppings' + clickCount + '"> \
+                       <input type="checkbox" name="toppings' + clickCount + '" value="pepperoni">Pepperoni<br> \
+                       <input type="checkbox" name="toppings' + clickCount + '" value="onion">Onion<br> \
+                       <input type="checkbox" name="toppings' + clickCount + '" value="olive">Olives<br> \
+                       <input type="checkbox" name="toppings' + clickCount + '" value="mushroom">Mushrooms<br> \
+                       <input type="checkbox" name="toppings' + clickCount + '" value="pepper">Peppers<br> \
+                       <input type="checkbox" name="toppings' + clickCount + '" value="bacon">Bacon<br> \
+                       <input type="checkbox" name="toppings' + clickCount + '" value="sausage">Sausage<br> \
+                       <input type="checkbox" name="toppings' + clickCount + '" value="tomato">Tomato<br> \
+                     </div> \
+                   </div> \
+                 </div> \
+              </div> \
+            </div>');
   });
 });
 
@@ -37,6 +94,7 @@ $(document).ready(function(){
 function Pizza(size, toppings) {
   this.diameter = size;
   this.toppings = toppings;
+  this.price = 0.00;
 }
 
 Pizza.prototype.cost = function() {
